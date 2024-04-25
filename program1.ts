0357de023d353ab3e447344533d3d675c522da4a
bilalbuttnazbutt <164246858+bilalbuttnazbutt@users.noreply.github.com>
2024-04-25 17:50:09 -0300

    Add files via upload

---
+++ program1.ts
@@ -0,0 +1,4 @@
+// NAZ
+// 17.March.2024
+// in this program we will write A.Z:
+console.log(`A B C D E F G H I J K L M N O P Q R S T U V W X Y Z`);
---
+++ program2.ts
@@ -0,0 +1,4 @@
+// NAZ
+// 17.March.2024
+// in this program we will write counting from 1.10;
+console.log(`1 2 3 4 5 6 7 8 9 10`);
